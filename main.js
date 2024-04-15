cd8e9d8ba233705f583437f4369d6a687e2d3109
Mostafa Mohamed Oraby <mostaaf.oraby159@gmail.com>
2024-04-15 18:02:59 +0200

    adding style

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
-const primaryHeader =   document.querySelector(".primary-header");
+const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-viible")
@@ -8,4 +8,20 @@ navToggle.addEventListener("click", () => {
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
+});
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  slidesToShow: 1,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    768: {
+      slidesToShow: 2,
+    },
+    1280: {
+      dots: false,
+      slidesToShow: 3,
+    },
+  },
 });
