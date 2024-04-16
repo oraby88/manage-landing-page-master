8c256f059f53d628978c9de3bc38196c85797689
Mostafa Mohamed Oraby <mostaaf.oraby159@gmail.com>
2024-04-16 18:03:17 +0200

    the leatest edit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const primaryNav = document.querySelector(".primary-navigation");
 const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute("data-viible")
+  primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
